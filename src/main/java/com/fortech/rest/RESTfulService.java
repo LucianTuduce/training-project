4002678b096c38d0160d2035b9a176572947d204
Lucian Tuduce <lucian.tuduce@fortech.ro>
2015-10-09 10:08:10 +0300

    Refactor

--- src/main/java/com/fortech/rest/RESTfulService.java
+++ src/main/java/com/fortech/rest/RESTfulService.java
@@ -1,9 +1,18 @@
 package com.fortech.rest;
 
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
+import com.fortech.model.MappingRule;
+import com.fortech.model.MarketRule;
+import com.fortech.model.MarketRulePK;
 import com.fortech.service.InterpretationRuleService;
 import com.fortech.service.MappingRuleService;
 import com.fortech.service.MarketRuleService;
@@ -27,5 +36,43 @@ public class RESTfulService {
 
 	@EJB
 	private InterpretationRuleService interpretationRuleService;
+
+	@POST
+	@Path("/add")
+	public Response insertInDatabase() {
+		MarketRule marketRule = new MarketRule();
+		marketRule.setActive((short) 1);
+		marketRule.setRule("Noua");
+		MarketRulePK marketRulePK = new MarketRulePK();
+		marketRulePK.setBranch(121);
+		marketRulePK.setCountryNumber("CJ-28-LUK");
+		marketRulePK.setStockCategory((short) 1);
+		marketRule.setId(marketRulePK);
+
+		MappingRule mappingRule = new MappingRule();
+		mappingRule.setId(4);
+		mappingRule.setSourceValue("2000");
+		mappingRule.setTargetValue("2400");
+		mappingRule.setVehicleAttribute("Quatro");
+
+		mappingRuleService.insertInDatabase(mappingRule);
+		marketRuleService.insertInDatabase(marketRule);
+
+		return Response.status(200).entity("INSERTEd").build();
+	}
+
+	@GET
+	@Path("/id")
+	@Produces("application/json")
+	public MappingRule getRule() {
+		return mappingRuleService.findById(4);
+	}
+
+	@GET
+	@Path("/all")
+	@Produces("application/json")
+	public List<MappingRule> getAllRule() {
+		return mappingRuleService.getAll();
+	}
 
 }
