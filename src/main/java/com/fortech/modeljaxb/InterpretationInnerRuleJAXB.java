bf673a63bbcddebfa6dd684a05ed80756e5d792e
Lucian Tuduce <lucian.tuduce@fortech.ro>
2015-10-13 18:03:19 +0300

    Merged branch master with branch lucian

--- src/main/java/com/fortech/modeljaxb/InterpretationInnerRuleJAXB.java
+++ src/main/java/com/fortech/modeljaxb/InterpretationInnerRuleJAXB.java
@@ -4,9 +4,6 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-
-import org.eclipse.persistence.oxm.annotations.XmlInverseReference;
 
 import com.fortech.model.InterpretationRule;
 
